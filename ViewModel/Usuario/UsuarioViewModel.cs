45256dd9d279ec509dbe528023b800cecd900150
May Dias <mayaradias023@gmail.com>
2024-11-24 16:36:06 -0300

    fix: correção do retorno com usuário, sem retornar a senha. Correção nas controllers, retornando a viewModel.

--- ViewModel/Usuario/UsuarioViewModel.cs
+++ ViewModel/Usuario/UsuarioViewModel.cs
@@ -5,7 +5,6 @@
         public long UsuarioId { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
-        public string Senha { get; set; }
         public string? Telefone { get; set; }
         public string? UsuarioRole { get; set; }
     }
