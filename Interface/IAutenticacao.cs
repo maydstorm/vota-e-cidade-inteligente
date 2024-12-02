3206feb5c7ee7eef9d5aebad87a8502af62a8bda
May Dias <mayaradias023@gmail.com>
2024-12-02 20:43:12 -0300

    remoção de arquivos duplicados

--- Interface/IAutenticacao.cs
+++
@@ -1,9 +0,0 @@
-﻿using VotaE_API.Models;
-
-namespace VotaE_API.Interface
-{
-    public interface IAutenticacao
-    {
-        UsuarioModel Authenticate(string email, string senha);
-    }
-}
--- ViewModel/Usuario/LoginViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VotaE_API.ViewModel.Usuario
-{
-    public class LoginViewModel
-    {
-        public string Email { get; set; }
-
-        public string Senha { get; set; }
-    }
-}
